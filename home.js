ac01adb58fb058f2e4d383d65ab4cc85a369e8e5
natanael <natan.pvc@gmail.com>
2023-11-23 08:51:21 -0300

    nova home atualizada

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 function mostranome(nome){
     return nome
 }
+
+function mostraSobreNome(sobreNome){
+    return sobreNome;
+}
+
+//sistema de login completo
+//Retornamos o nome  do usuario quando logado
--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 //trabalhando no login
 
 //finalizando o sistema de login
+
+function login(nome, email){
+    return 'bem vindo';
+
+}
+
+// seja bem vindo
